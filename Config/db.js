5c08f1e4d48b74f65c3427945849a6c3dbad612e
Justin Stauffer <justindstauffer@gmail.com>
2021-12-05 11:59:14 -0500

    Update db.js

--- Config/db.js
+++ Config/db.js
@@ -1,5 +1,5 @@
-const dbURL = "mongodb+srv://justinstauffer:SpoolsDevTest1234@spoolsdev.uv2om.mongodb.net/<dbname>?retryWrites=true&w=majority"
+const dbURL = <MongoDB URL>
 
-const jwtSecret = "J0e00177d817054395059786e832d01e"
+const jwtSecret = <JWT Secret>
 
 module.exports = dbURL
